86cc7ae152fde4e1a2be9d37790827232d2d431c
Fernando Silveira <fsilveira@gmail.com>
2017-03-22 23:45:52 -0300

    Goodbye Allegro, hello SDL2!

--- main.cpp
+++ main.cpp
@@ -1,126 +1,37 @@
-#include "game.hpp"
+#include <SDL.h>
 
-#include "allegro/event.hpp"
+#include <err.h>
+#include <stdio.h>
 
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_primitives.h>
-
-#include <iostream>
-
-#define FREQ_UPDATE 1. / 30
-#define FREQ_FRAME 1. / 60
-
-using namespace cr;
-
-void
-main_loop(ALLEGRO_EVENT_QUEUE *event_queue, ALLEGRO_TIMER *frame_timer)
-{
-    game game;
-    allegro::event event;
-    bool running = true;
-    double last_update_time, current_time;
-    const double minimum_update_frequency = FREQ_UPDATE;
-
-    current_time = al_get_time();
-
-    game.init_update(current_time);
-    game.draw();
-    al_flip_display();
-    last_update_time = current_time;
-
-    while (running) {
-        al_wait_for_event(event_queue, &event.get());
-
-        current_time = al_get_time();
-
-        switch (event.get().type) {
-        case ALLEGRO_EVENT_DISPLAY_CLOSE:
-            running = false;
-            break;
-        case ALLEGRO_EVENT_KEY_DOWN:
-            if (event.get().keyboard.keycode == ALLEGRO_KEY_ESCAPE)
-                running = false;
-            break;
-        case ALLEGRO_EVENT_TIMER:
-            for (;;) {
-                double update_time = last_update_time + minimum_update_frequency;
-                if (update_time >= current_time)
-                    break;
-                game.update(nullptr, update_time);
-                last_update_time = update_time;
-            }
-
-            if (event.get().timer.source == frame_timer) {
-                game.update(&event, current_time);
-                game.draw();
-                al_flip_display();
-                last_update_time = current_time;
-            }
-
-            break;
-        }
-    }
-}
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int
-main()
+main(int argc, char *argv[])
 {
-    if (!al_init()) {
-        std::cerr << "error initializing allegro" << std::endl;
-        return 1;
-    }
+    SDL_Window *window = NULL;
+    SDL_Surface *screen_surface = NULL;
 
-    ALLEGRO_DISPLAY *display = al_create_display(640, 480);
-    if (display == nullptr) {
-        std::cerr << "error creating allegro display" << std::endl;
-        return 1;
-    }
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+        errx(EXIT_FAILURE, "error initializing sdl: %s", SDL_GetError());
 
-    if (!al_install_keyboard()) {
-        std::cerr << "error installing allegro keyboard" << std::endl;
-        return 1;
-    }
+    window = SDL_CreateWindow("Crace Razy", SDL_WINDOWPOS_UNDEFINED,
+            SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT,
+            SDL_WINDOW_SHOWN);
+    if (window == NULL)
+        errx(EXIT_FAILURE, "error creating sdl window: %s", SDL_GetError());
 
-    if (!al_init_primitives_addon()) {
-        std::cerr << "error initializing allegro primitives addon" << std::endl;
-        return 1;
-    }
+    screen_surface = SDL_GetWindowSurface(window);
 
-    ALLEGRO_TIMER *timer = al_create_timer(FREQ_FRAME);
-    if (display == nullptr) {
-        std::cerr << "error creating allegro timer" << std::endl;
-        return 1;
-    }
+    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0, 100, 50));
 
-    ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
-    if (event_queue == nullptr) {
-        std::cerr << "error creating allegro event queue" << std::endl;
-        return 1;
-    }
+    SDL_UpdateWindowSurface(window);
 
-    al_register_event_source(event_queue, al_get_keyboard_event_source());
-    al_register_event_source(event_queue, al_get_timer_event_source(timer));
-    al_register_event_source(event_queue, al_get_display_event_source(display));
+    SDL_Delay(2000);
 
-    al_start_timer(timer);
+    SDL_DestroyWindow(window);
 
-    main_loop(event_queue, timer);
-
-    al_stop_timer(timer);
-
-    al_unregister_event_source(event_queue, al_get_display_event_source(display));
-    al_unregister_event_source(event_queue, al_get_timer_event_source(timer));
-    al_unregister_event_source(event_queue, al_get_keyboard_event_source());
-
-    al_destroy_event_queue(event_queue);
-    al_destroy_timer(timer);
-
-    al_shutdown_primitives_addon();
-    al_uninstall_keyboard();
-
-    al_destroy_display(display);
-
-    al_uninstall_system();
+    SDL_Quit();
 
     return 0;
 }
--- object.cpp
+++
@@ -1,9 +0,0 @@
-#include "object.hpp"
-
-using namespace cr;
-
-object::~object()
-{
-}
-
-// vim:set sw=4 ts=4 et:
